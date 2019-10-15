011680488632bdd2b7aa5347b531dd9548f130ae
Federico Ficarelli <federico.ficarelli@gmail.com>
2019-10-15 10:18:30 +0200

    Remove sanitizer build tyoes, add presets

---
+++ cmake/presets/clang-native-libc++.cmake
@@ -0,0 +1,17 @@
+# Auto-detect arch of the build machine and optimize/generate for it
+
+set(arch "-march=native -mtune=native")
+
+# Release
+set(CMAKE_C_FLAGS_RELEASE "${arch} -O3 -DNDEBUG" CACHE STRING "")
+set(CMAKE_CXX_FLAGS_RELEASE "${arch} -O3 -DNDEBUG -stdlib=libc++" CACHE STRING "")
+
+# RelWithDebInfo
+set(CMAKE_C_FLAGS_RELWITHDEBINFO "${arch} -g -O2 -DNDEBUG" CACHE STRING "")
+set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "${arch} -g -O2 -DNDEBUG -stdlib=libc++" CACHE STRING "")
+
+# Debug
+set(CMAKE_C_FLAGS_DEBUG "${arch} -g -O0" CACHE STRING "")
+set(CMAKE_CXX_FLAGS_DEBUG "${arch} -g -O0 -stdlib=libc++" CACHE STRING "")
+
+unset(arch)
---
+++ cmake/presets/native.cmake
@@ -0,0 +1,13 @@
+# Auto-detect arch of the build machine and optimize/generate for it
+
+set(opt "-O3 -DNDEBUG -march=native -mtune=native")
+
+# Release
+set(CMAKE_C_FLAGS_RELEASE "${opt}" CACHE STRING "")
+set(CMAKE_CXX_FLAGS_RELEASE "${opt}" CACHE STRING "")
+
+# RelWithDebInfo
+set(CMAKE_C_FLAGS_RELWITHDEBINFO "${opt} -g" CACHE STRING "")
+set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "${opt} -g" CACHE STRING "")
+
+unset(opt)
