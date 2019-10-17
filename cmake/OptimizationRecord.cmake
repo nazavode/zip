fef8a14e7adb5d8f82790f825580c0b18a44aa4c
Federico Ficarelli <federico.ficarelli@gmail.com>
2019-10-18 00:51:27 +0200

    WIP optrecord

--- cmake/OptimizationRecord.cmake
+++ cmake/OptimizationRecord.cmake
@@ -1,46 +1,27 @@
-
-# clang++ -I/home/montag/src/zip/include -O3 -DNDEBUG -march=skylake-avx512 -mtune=skylake-avx512 -Wall -Wextra -Werror -std=c++1z -S -fsave-optimization-record -Rpass-analysis=loop-vectorize -Rpass=loop-vectorize -Rpass-missed=loop-vectorize -c Point3dSoaZipSum.cpp
-
-set(OPTIMIZATION_RECORD_FLAGS -fsave-optimization-record
-
-include(CheckCXXCompilerFlag)
-
-check_cxx_compiler_flag(-fsave-optimization-record ZIP_HAS_SAVE_OPTIMIZATION_RECORD_FLAGS)
-
-check_cxx_compiler_flag(-g0 BENCHMARK_HAS_G0_FLAG)
-if (BENCHMARK_HAS_G0_FLAG)
-  list(APPEND ASM_TEST_FLAGS -g0)
+if(__optimization_record)
+  return()
 endif()
+set(__optimization_record INCLUDED)
 
-check_cxx_compiler_flag(-fno-stack-protector BENCHMARK_HAS_FNO_STACK_PROTECTOR_FLAG)
-if (BENCHMARK_HAS_FNO_STACK_PROTECTOR_FLAG)
-  list(APPEND ASM_TEST_FLAGS -fno-stack-protector)
-endif()
+set(OPTRECORD_CXXFLAGS "-O3 -DNDEBUG -std=c++17" CACHE INTERNAL "")
+set(OPTRECORD_ARCH "skylake-avx512" CACHE INTERNAL "")
+set(OPTRECORD_PASSES "[a-z]*" CACHE INTERNAL "")
 
-split_list(ASM_TEST_FLAGS)
-string(TOUPPER "${CMAKE_CXX_COMPILER_ID}" ASM_TEST_COMPILER)
-
-macro(add_optimization_test source)
-
-  cmake_parse_arguments(ARG "" "" "CHECK_PREFIXES" ${ARGV})
-  add_library(${name} OBJECT ${name}.cc)
-  set_target_properties(${name} PROPERTIES COMPILE_FLAGS "-S ${ASM_TEST_FLAGS}")
-  set(ASM_OUTPUT_FILE "${CMAKE_CURRENT_BINARY_DIR}/${name}.s")
-  add_custom_target(copy_${name} ALL
-      COMMAND ${PROJECT_SOURCE_DIR}/tools/strip_asm.py
-        $<TARGET_OBJECTS:${name}>
-        ${ASM_OUTPUT_FILE}
-      BYPRODUCTS ${ASM_OUTPUT_FILE})
-  add_dependencies(copy_${name} ${name})
-  if (NOT ARG_CHECK_PREFIXES)
-    set(ARG_CHECK_PREFIXES "CHECK")
-  endif()
-  foreach(prefix ${ARG_CHECK_PREFIXES})
-    add_test(NAME run_${name}_${prefix}
-        COMMAND
-          ${LLVM_FILECHECK_EXE} ${name}.cc
-          --input-file=${ASM_OUTPUT_FILE}
-          --check-prefixes=CHECK,CHECK-${ASM_TEST_COMPILER}
-        WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
+function(add_optimization_record out_var)
+  set(result)
+  foreach(in_f ${ARGN})
+    # file(RELATIVE_PATH out_f ${CMAKE_CURRENT_SOURCE_DIR} ${in_f})
+    get_filename_component(basename ${in_f} NAME_WE)
+    set(asm_out_f "${CMAKE_CURRENT_BINARY_DIR}/${basename}.s")
+    set(optrecord_out_f "${CMAKE_CURRENT_BINARY_DIR}/${basename}.opt.yaml")
+    add_custom_command(OUTPUT ${optrecord_out_f}
+      COMMAND ${CMAKE_CXX_COMPILER} -S ${OPTRECORD_CXXFLAGS} -march=${OPTRECORD_ARCH} -mtune=${OPTRECORD_ARCH} -fsave-optimization-record -foptimization-record-file=${optrecord_out_f} -o ${asm_out_f} -c ${in_f}
+      DEPENDS ${in_f}
+      WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+      COMMENT "Generating optimization record for ${in_f}"
+      VERBATIM
+      )
+    list(APPEND result ${optrecord_out_f})
   endforeach()
-endmacro()
+  set(${out_var} "${result}" PARENT_SCOPE)
+endfunction()
