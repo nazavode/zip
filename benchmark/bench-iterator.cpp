85604afc1d701bd27cea7dc03652c2685200f4a7
Federico Ficarelli <federico.ficarelli@gmail.com>
2019-10-17 17:32:11 +0200

    Polish benchmarks

--- benchmark/bench-iterator.cpp
+++ benchmark/bench-iterator.cpp
@@ -2,7 +2,145 @@
 
 #include <zip.h>
 
+#include <numeric>
 #include <vector>
+
+class Point3DSoA : public ::benchmark::Fixture {
+   public:
+   
+    using value_type = int;
+    using container_type = std::vector<value_type>;
+
+    void SetUp(const ::benchmark::State& state) {
+        x.resize(state.range(0));
+        y.resize(state.range(0));
+        z.resize(state.range(0));
+        std::iota(std::begin(x), std::end(x), value_type{});
+        std::iota(std::begin(y), std::end(y), value_type{});
+        std::iota(std::begin(z), std::end(z), value_type{});
+    }
+
+    void TearDown(::benchmark::State& state) {
+        state.SetBytesProcessed(
+            static_cast<int64_t>(
+                state.iterations() * state.range(0) * sizeof(value_type) * 3));
+        x.clear();
+        y.clear();
+        z.clear();
+    }
+    
+    constexpr auto size() const noexcept {
+        return std::size(x);
+    }
+
+    constexpr auto cbegin() const noexcept {
+        return zip::zip_iterator{std::cbegin(x), std::cbegin(y), std::cbegin(z)};
+    }
+
+    constexpr auto cend() const noexcept {
+        return zip::zip_iterator{std::cend(x), std::cend(y), std::cend(z)};
+    }
+
+    container_type x;
+    container_type y;
+    container_type z;
+};
+
+// Baseline for 3d points summation:
+// accumulator variables have been clobberedd to prevent
+// any kind of vectorization
+BENCHMARK_DEFINE_F(Point3DSoA, Baseline)(benchmark::State& state) {
+    for (auto _ : state) {
+        auto sum_x = value_type{};
+        auto sum_y = value_type{};
+        auto sum_z = value_type{};
+
+        for(decltype(size()) i = 0; i < size(); ++i) {
+            sum_x += x[i];
+            sum_y += y[i];
+            sum_z += z[i];
+        }
+
+        benchmark::DoNotOptimize(sum_x);
+        benchmark::DoNotOptimize(sum_y);
+        benchmark::DoNotOptimize(sum_z);
+    }
+}
+BENCHMARK_REGISTER_F(Point3DSoA, Baseline)->Range(1<<0, 1<<10);
+
+BENCHMARK_DEFINE_F(Point3DSoA, SubscriptSum)(benchmark::State& state) {
+    for (auto _ : state) {
+        auto sum_x = value_type{};
+        auto sum_y = value_type{};
+        auto sum_z = value_type{};
+
+        for(decltype(size()) i = 0; i < size(); ++i) {
+            sum_x += x[i];
+            sum_y += y[i];
+            sum_z += z[i];
+        }
+
+        auto return_sum_x = sum_x;
+        auto return_sum_y = sum_y;
+        auto return_sum_z = sum_z;
+        benchmark::DoNotOptimize(return_sum_x);
+        benchmark::DoNotOptimize(return_sum_y);
+        benchmark::DoNotOptimize(return_sum_z);
+    }
+}
+BENCHMARK_REGISTER_F(Point3DSoA, SubscriptSum)->Range(1<<0, 1<<10);
+
+BENCHMARK_DEFINE_F(Point3DSoA, IteratorSum)(benchmark::State& state) {
+    for (auto _ : state) {
+        auto sum_x = value_type{};
+        auto sum_y = value_type{};
+        auto sum_z = value_type{};
+
+        auto it_x = std::cbegin(x);
+        auto it_y = std::cbegin(y);
+        auto it_z = std::cbegin(z);
+        const auto end = std::cend(x);
+
+        while(it_x != end) {
+            sum_x += *it_x++;
+            sum_y += *it_y++;
+            sum_z += *it_z++;
+        }
+
+        auto return_sum_x = sum_x;
+        auto return_sum_y = sum_y;
+        auto return_sum_z = sum_z;
+        benchmark::DoNotOptimize(return_sum_x);
+        benchmark::DoNotOptimize(return_sum_y);
+        benchmark::DoNotOptimize(return_sum_z);
+    }
+}
+BENCHMARK_REGISTER_F(Point3DSoA, IteratorSum)->Range(1<<0, 1<<10);
+
+BENCHMARK_DEFINE_F(Point3DSoA, ZipSum)(benchmark::State& state) {
+    for (auto _ : state) {
+        auto sum_x = value_type{};
+        auto sum_y = value_type{};
+        auto sum_z = value_type{};
+
+        for(auto it = cbegin(); it != cend(); ++it) {
+            const auto [value_x, value_y, value_z] = *it;
+            sum_x += value_x;
+            sum_y += value_y;
+            sum_z += value_z;
+        }
+
+        auto return_sum_x = sum_x;
+        auto return_sum_y = sum_y;
+        auto return_sum_z = sum_z;
+        benchmark::DoNotOptimize(return_sum_x);
+        benchmark::DoNotOptimize(return_sum_y);
+        benchmark::DoNotOptimize(return_sum_z);
+    }
+}
+BENCHMARK_REGISTER_F(Point3DSoA, ZipSum)->Range(1<<0, 1<<10);
+
+////////////////////////////////////////////////////////////////////
 
 static void BM_1DSubscriptSumVectorNoSIMD(benchmark::State& state) {
     const std::vector<int> x(state.range(0), state.range(0));
@@ -20,248 +158,6 @@ static void BM_1DSubscriptSumVectorNoSIMD(benchmark::State& state) {
         static_cast<int64_t>(state.iterations() * state.range(0) * sizeof(int)));
 }
 BENCHMARK(BM_1DSubscriptSumVectorNoSIMD)->Range(1<<0, 1<<10);
-
-
-static void BM_1DSubscriptSumVector(benchmark::State& state) {
-    const std::vector<int> x(state.range(0), state.range(0));
-
-    for (auto _ : state) {
-        int sum_x = 0;
-
-        for(std::size_t i = 0; i < std::size(x); ++i) {
-            sum_x += x[i];
-        }
-        int return_sum_x = sum_x;
-        benchmark::DoNotOptimize(return_sum_x);
-    }
-    
-    state.SetBytesProcessed(
-        static_cast<int64_t>(state.iterations() * state.range(0) * sizeof(int)));
-}
-BENCHMARK(BM_1DSubscriptSumVector)->Range(1<<0, 1<<10);
-
-static void BM_1DIteratorSumVector(benchmark::State& state) {
-    const std::vector<int> x(state.range(0), state.range(0));
-
-    for (auto _ : state) {
-        int sum_x = 0;
-
-        auto it_x = std::cbegin(x);
-        
-        const auto end = std::cend(x);
-
-        while(it_x != end) {
-            sum_x += *it_x++;
-        }
-
-        int return_sum_x = sum_x;
-        benchmark::DoNotOptimize(return_sum_x);
-    }
-    
-    state.SetBytesProcessed(
-        static_cast<int64_t>(state.iterations() * state.range(0) * sizeof(int)));
-}
-BENCHMARK(BM_1DIteratorSumVector)->Range(1<<0, 1<<10);
-
-
-static void BM_1DZipSumVector(benchmark::State& state) {
-    const std::vector<int> x(state.range(0), state.range(0));
-
-    for (auto _ : state) {
-
-        int sum_x = 0;
-
-        const auto end = zip::zip_iterator{std::cend(x)};
-
-        for(auto it = zip::zip_iterator{std::cbegin(x)}; it != end; ++it) {
-            const auto [value_x] = *it; 
-            sum_x += value_x;
-        }
-
-        int return_sum_x = sum_x;
-        benchmark::DoNotOptimize(return_sum_x);
-    }
-    
-    state.SetBytesProcessed(
-        static_cast<int64_t>(state.iterations() * state.range(0) * sizeof(int)));
-}
-BENCHMARK(BM_1DZipSumVector)->Range(1<<0, 1<<10);
-
-static void BM_2DSubscriptSumVector(benchmark::State& state) {
-    const std::vector<int> x(state.range(0), state.range(0));
-    const std::vector<int> y(state.range(0), state.range(0));
-
-    for (auto _ : state) {
-        int sum_x = 0;
-        int sum_y = 0;
-
-        for(std::size_t i = 0; i < std::size(x); ++i) {
-            sum_x += x[i];
-            sum_y += y[i];
-        }
-
-        int return_sum_x = sum_x;
-        int return_sum_y = sum_y;
-        benchmark::DoNotOptimize(return_sum_x);
-        benchmark::DoNotOptimize(return_sum_y);
-    }
-    
-    state.SetBytesProcessed(
-        static_cast<int64_t>(state.iterations() * state.range(0) * sizeof(int) * 2));
-}
-BENCHMARK(BM_2DSubscriptSumVector)->Range(1<<0, 1<<10);
-
-static void BM_2DIteratorSumVector(benchmark::State& state) {
-    const std::vector<int> x(state.range(0), state.range(0));
-    const std::vector<int> y(state.range(0), state.range(0));
-
-    for (auto _ : state) {
-        int sum_x = 0;
-        int sum_y = 0;
-
-        auto it_x = std::cbegin(x);
-        auto it_y = std::cbegin(y);
-        const auto end = std::cend(x);
-
-        while(it_x != end) {
-            sum_x += *it_x++;
-            sum_y += *it_y++;
-        }
-
-        int return_sum_x = sum_x;
-        int return_sum_y = sum_y;
-        benchmark::DoNotOptimize(return_sum_x);
-        benchmark::DoNotOptimize(return_sum_y);
-    }
-    
-    state.SetBytesProcessed(
-        static_cast<int64_t>(state.iterations() * state.range(0) * sizeof(int) * 2));
-}
-BENCHMARK(BM_2DIteratorSumVector)->Range(1<<0, 1<<10);
-
-static void BM_2DZipSumVector(benchmark::State& state) {
-    const std::vector<int> x(state.range(0), state.range(0));
-    const std::vector<int> y(state.range(0), state.range(0));
-
-    for (auto _ : state) {
-
-        int sum_x = 0;
-        int sum_y = 0;
-
-        const auto end = zip::zip_iterator{std::cend(x), std::cend(y)};
-
-        for(auto it = zip::zip_iterator{std::cbegin(x), std::cbegin(y)}; it != end; ++it) {
-            const auto [value_x, value_y] = *it; 
-            sum_x += value_x;
-            sum_y += value_y;
-        }
-
-        int return_sum_x = sum_x;
-        int return_sum_y = sum_y;
-        benchmark::DoNotOptimize(return_sum_x);
-        benchmark::DoNotOptimize(return_sum_y);
-    }
-    
-    state.SetBytesProcessed(
-        static_cast<int64_t>(state.iterations() * state.range(0) * sizeof(int) * 2));
-}
-BENCHMARK(BM_2DZipSumVector)->Range(1<<0, 1<<10);
-
-static void BM_3DSubscriptSumVector(benchmark::State& state) {
-    const std::vector<int> x(state.range(0), state.range(0));
-    const std::vector<int> y(state.range(0), state.range(0));
-    const std::vector<int> z(state.range(0), state.range(0));
-
-    for (auto _ : state) {
-        int sum_x = 0;
-        int sum_y = 0;
-        int sum_z = 0;
-
-        for(std::size_t i = 0; i < std::size(x); ++i) {
-            sum_x += x[i];
-            sum_y += y[i];
-            sum_z += z[i];
-        }
-
-        int return_sum_x = sum_x;
-        int return_sum_y = sum_y;
-        int return_sum_z = sum_z;
-        benchmark::DoNotOptimize(return_sum_x);
-        benchmark::DoNotOptimize(return_sum_y);
-        benchmark::DoNotOptimize(return_sum_z);
-    }
-    
-    state.SetBytesProcessed(
-        static_cast<int64_t>(state.iterations() * state.range(0) * sizeof(int) * 3));
-}
-BENCHMARK(BM_3DSubscriptSumVector)->Range(1<<0, 1<<10);
-
-static void BM_3DIteratorSumVector(benchmark::State& state) {
-    const std::vector<int> x(state.range(0), state.range(0));
-    const std::vector<int> y(state.range(0), state.range(0));
-    const std::vector<int> z(state.range(0), state.range(0));
-
-    for (auto _ : state) {
-        int sum_x = 0;
-        int sum_y = 0;
-        int sum_z = 0;
-
-        auto it_x = std::cbegin(x);
-        auto it_y = std::cbegin(y);
-        auto it_z = std::cbegin(z);
-        const auto end = std::cend(x);
-
-        while(it_x != end) {
-            sum_x += *it_x++;
-            sum_y += *it_y++;
-            sum_z += *it_z++;
-        }
-
-        int return_sum_x = sum_x;
-        int return_sum_y = sum_y;
-        int return_sum_z = sum_z;
-        benchmark::DoNotOptimize(return_sum_x);
-        benchmark::DoNotOptimize(return_sum_y);
-        benchmark::DoNotOptimize(return_sum_z);
-    }
-    
-    state.SetBytesProcessed(
-        static_cast<int64_t>(state.iterations() * state.range(0) * sizeof(int) * 3));
-}
-BENCHMARK(BM_3DIteratorSumVector)->Range(1<<0, 1<<10);
-
-static void BM_3DZipSumVector(benchmark::State& state) {
-    const std::vector<int> x(state.range(0), state.range(0));
-    const std::vector<int> y(state.range(0), state.range(0));
-    const std::vector<int> z(state.range(0), state.range(0));
-
-    for (auto _ : state) {
-
-        int sum_x = 0;
-        int sum_y = 0;
-        int sum_z = 0;
-
-        const auto end = zip::zip_iterator{std::cend(x), std::cend(y), std::cend(z)};
-
-        for(auto it = zip::zip_iterator{std::cbegin(x), std::cbegin(y), std::cbegin(z)}; it != end; ++it) {
-            const auto [value_x, value_y, value_z] = *it; 
-            sum_x += value_x;
-            sum_y += value_y;
-            sum_z += value_z;
-        }
-
-        int return_sum_x = sum_x;
-        int return_sum_y = sum_y;
-        int return_sum_z = sum_z;
-        benchmark::DoNotOptimize(return_sum_x);
-        benchmark::DoNotOptimize(return_sum_y);
-        benchmark::DoNotOptimize(return_sum_z);
-    }
-    
-    state.SetBytesProcessed(
-        static_cast<int64_t>(state.iterations() * state.range(0) * sizeof(int) * 3));
-}
-BENCHMARK(BM_3DZipSumVector)->Range(1<<0, 1<<10);
 
 static void BM_3DSubscriptSumVectorAoS(benchmark::State& state) {
     struct Item {
