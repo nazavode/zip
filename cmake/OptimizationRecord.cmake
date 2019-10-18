a81fd9e99a01c3d1a5f747e732207690ab7372ab
Federico Ficarelli <federico.ficarelli@gmail.com>
2019-10-18 14:58:01 +0200

    Add optrecord

--- cmake/OptimizationRecord.cmake
+++ cmake/OptimizationRecord.cmake
@@ -1,27 +1,70 @@
-if(__optimization_record)
-  return()
-endif()
-set(__optimization_record INCLUDED)
+set(OPTRECORD_DEFAULT_COMPILER        ${CMAKE_CXX_COMPILER} CACHE STRING "Default compiler used for optimization record generation when no COMPILER argument is provided")
+set(OPTRECORD_DEFAULT_COMPILER_FLAGS  -O3;-DNDEBUG CACHE STRING "Default compiler flags used for optimization record generation when no COMPILER_FLAGS argument is provided")
+set(OPTRECORD_DEFAULT_ARCH            x86-64 CACHE STRING "Default code generation architecture to be targeted when no ARCH argument is provided")
+set(OPTRECORD_DEFAULT_COMPILER_PASSES [a-z]* CACHE STRING "Default regular expression used by the compiler to select compiler passes to be included in the record output")
 
-set(OPTRECORD_CXXFLAGS "-O3 -DNDEBUG -std=c++17" CACHE INTERNAL "")
-set(OPTRECORD_ARCH "skylake-avx512" CACHE INTERNAL "")
-set(OPTRECORD_PASSES "[a-z]*" CACHE INTERNAL "")
-
-function(add_optimization_record out_var)
-  set(result)
-  foreach(in_f ${ARGN})
-    # file(RELATIVE_PATH out_f ${CMAKE_CURRENT_SOURCE_DIR} ${in_f})
-    get_filename_component(basename ${in_f} NAME_WE)
-    set(asm_out_f "${CMAKE_CURRENT_BINARY_DIR}/${basename}.s")
-    set(optrecord_out_f "${CMAKE_CURRENT_BINARY_DIR}/${basename}.opt.yaml")
-    add_custom_command(OUTPUT ${optrecord_out_f}
-      COMMAND ${CMAKE_CXX_COMPILER} -S ${OPTRECORD_CXXFLAGS} -march=${OPTRECORD_ARCH} -mtune=${OPTRECORD_ARCH} -fsave-optimization-record -foptimization-record-file=${optrecord_out_f} -o ${asm_out_f} -c ${in_f}
-      DEPENDS ${in_f}
-      WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
-      COMMENT "Generating optimization record for ${in_f}"
-      VERBATIM
-      )
-    list(APPEND result ${optrecord_out_f})
-  endforeach()
-  set(${out_var} "${result}" PARENT_SCOPE)
+function(add_optimization_record TARGET)
+    set(oneValueArgs
+        COMPILER
+        DESTINATION
+        ARCH
+        COMPILER_PASSES)
+    set(multiValueArgs
+        SOURCES
+        COMPILER_FLAGS
+        INCLUDE_DIRECTORIES)
+    # Parse args:
+    cmake_parse_arguments(PARSE_ARGV 1 arg 
+        "${options}" "${oneValueArgs}" "${multiValueArgs}")
+    # Set fallback values with defaults from cache:
+    if(NOT arg_COMPILER)
+        set(arg_COMPILER ${OPTRECORD_DEFAULT_COMPILER})
+    endif()
+    if(NOT arg_COMPILER_FLAGS)
+        set(arg_COMPILER_FLAGS ${OPTRECORD_DEFAULT_COMPILER_FLAGS})
+    endif()
+    if(NOT arg_ARCH)
+        set(arg_ARCH ${OPTRECORD_DEFAULT_ARCH})
+    endif()
+    if(NOT arg_COMPILER_PASSES)
+        set(arg_COMPILER_PASSES ${OPTRECORD_DEFAULT_COMPILER_PASSES})
+    endif()
+    # Make sure destination directory exists:
+    file(MAKE_DIRECTORY ${arg_DESTINATION})
+    # Preprocess include directories to produce correct include flags:
+    set(includes "")
+    foreach(dir ${arg_INCLUDE_DIRECTORIES})
+        list(APPEND includes "-I${dir}")
+    endforeach()
+    # Generated file list, use to trigger dependencies:
+    set(result)
+    foreach(in_f ${arg_SOURCES})
+        get_filename_component(basename ${in_f} NAME_WE)
+        set(asm_out_f "${arg_DESTINATION}/${basename}.s")
+        set(optrecord_out_f "${arg_DESTINATION}/${basename}.opt.yaml")
+        set(command
+            ${arg_COMPILER} ${arg_COMPILER_FLAGS} -S
+            ${includes}
+            -march=${arg_ARCH}
+            -mtune=${arg_ARCH}
+            -fsave-optimization-record
+            -foptimization-record-passes='${arg_COMPILER_PASSES}'
+            -foptimization-record-file=${optrecord_out_f}
+            -o ${asm_out_f} -c ${in_f})
+        add_custom_command(
+            OUTPUT ${optrecord_out_f} ${asm_out_f}
+            COMMAND ${command}
+            DEPENDS ${in_f}
+            WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+            COMMENT "Generating optimization record for ${basename}"
+            COMMAND_EXPAND_LISTS)
+        list(APPEND result ${optrecord_out_f} ${asm_out_f})
+        # Emit generation log
+        set(log_out_f "${arg_DESTINATION}/${basename}.opt.log")
+        string(REPLACE ";" " " log_content "${command}")
+        file(WRITE ${log_out_f} "${log_content}")
+    endforeach()
+    # Add phony target to trigger all the generated
+    # custom commands:
+    add_custom_target(${TARGET} ALL DEPENDS ${result})
 endfunction()
