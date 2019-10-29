284b003e3bf235a86c3fa4429ee1a9f7a37a4f54
Federico Ficarelli <federico.ficarelli@gmail.com>
2019-10-29 18:06:58 +0100

    Fix optreport

--- cmake/FnAddOptReport.cmake
+++ cmake/FnAddOptReport.cmake
@@ -5,8 +5,8 @@
 # https://software.intel.com/en-us/articles/getting-the-most-out-of-your-intel-compiler-with-the-new-optimization-reports
 
 include(FnGetTargetIncludeDirectories)
+include(CheckCXXCompilerFlag)
 
-# set(OPTREPORT_DEFAULT_COMPILER         CACHE STRING "Default compiler used for optimization record generation when no COMPILER argument is provided")
 set(OPTREPORT_DEFAULT_COMPILER_FLAGS  ${CMAKE_CXX_FLAGS_RELEASE} CACHE STRING "Default compiler flags used for optimization record generation when no COMPILER_FLAGS argument is provided")
 set(OPTREPORT_DEFAULT_ARCH            x86-64 CACHE STRING "Default code generation architecture to be targeted when no ARCH argument is provided")
 set(OPTREPORT_DEFAULT_PASS_FILTER     [a-z]* CACHE STRING "Default regular expression used by the compiler to select compiler passes to be included in the record output")
@@ -16,6 +16,10 @@ mark_as_advanced(
     OPTREPORT_DEFAULT_ARCH
     OPTREPORT_DEFAULT_PASS_FILTER)
 
+# Note to self: no way we are using generator expressions
+# here, any attempt is going to end up in:
+# "$<CXX_COMPILER_ID> may only be used with binary targets"
+# Thank you CMake, brilliant as usual.
 
 function(get_optreport_flag OUTVAR)
     set(flags)
@@ -34,11 +38,11 @@ endfunction(get_optreport_flag OUTVAR)
 function(get_optreport_outfile_flag OUTVAR DESTINATION)
     set(flags)
     if(CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
-        list(APPEND flags "-foptimization-record-file='${REPORT_OUTFILE}'")
+        list(APPEND flags "-foptimization-record-file='${DESTINATION}'")
     elseif(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
         # TODO Does GNU has something for this?
     elseif(CMAKE_CXX_COMPILER_ID STREQUAL "Intel")
-        list(APPEND flags "-qopt-report-file='${REPORT_OUTFILE}'")
+        list(APPEND flags "-qopt-report-file='${DESTINATION}'")
     endif()
     set(${OUTVAR} ${flags} PARENT_SCOPE)
 endfunction()
@@ -63,15 +67,15 @@ function(get_optreport_pass_filter_flag OUTVAR FILTER)
 endfunction(get_optreport_pass_filter_flag OUTVAR)
 
 
-function(add_optimization_report TARGET)
+function(add_optreport TARGET)
     set(oneValueArgs
-        DESTINATION
-        ARCH
-        COMPILER_PASSES)
+            DESTINATION
+            ARCH
+            COMPILER_PASSES)
     set(multiValueArgs
-        SOURCES
-        COMPILER_FLAGS
-        INCLUDES)
+            SOURCES
+            COMPILER_FLAGS
+            INCLUDES)
     # Parse args:
     cmake_parse_arguments(PARSE_ARGV 1 arg 
         "${options}" "${oneValueArgs}" "${multiValueArgs}")
@@ -103,17 +107,23 @@ function(add_optimization_report TARGET)
     list(APPEND flags ${_flg})
 
     # Preprocess include directories to produce correct include flags:
+    set(include_items "")
     foreach(item ${arg_INCLUDES})
         if(TARGET ${item})
             # If the current item is a target, extract its
             # include directories:
-            get_target_include_directories(tgt_includes ${item})
-            foreach(_inc ${tgt_includes})
-                list(APPEND flags "-I${_inc}")
-            endforeach()
+            get_target_include_directories(tgt_include_items ${item})
+            list(APPEND include_items ${tgt_include_items})
         else()
-            list(APPEND flags "-I${dir}")
+            list(APPEND include_items "${item}")
         endif()
+    endforeach()
+    # Sanitize list: remove generator expressions with empty values
+    # that would end up as valueless "-I" on the command line:
+    string(REGEX REPLACE "\\$\<[^:]+:\>" "" include_items "${include_items}")
+    # list(TRANSFORM) only in CMake >= 12 :(
+    foreach(item ${include_items})
+        list(APPEND flags "-I${item}")
     endforeach()
 
     # Make sure destination directory exists:
@@ -122,25 +132,28 @@ function(add_optimization_report TARGET)
     # Generated file list, used to trigger dependencies:
     set(result)
     foreach(in_f ${arg_SOURCES})
-        get_filename_component(basename ${in_f} NAME_WE)
-        set(asm_out_f "${arg_DESTINATION}/${basename}.s")
-        get_optreport_outfile_flag(optreport_out_f "${arg_DESTINATION}/${basename}.opt")
-        set(command
-            ${CMAKE_CXX_COMPILER} ${flags}
-            ${optreport_out_f}
-            -o ${asm_out_f} -c ${in_f})
-        add_custom_command(
-            OUTPUT ${optreport_out_f} ${asm_out_f}
-            COMMAND ${command}
-            DEPENDS ${in_f}
-            WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
-            COMMENT "Generating optimization record for ${basename}"
-            COMMAND_EXPAND_LISTS)
-        list(APPEND result ${optrePORT_out_f} ${asm_out_f})
         # Emit generation log
         set(log_out_f "${arg_DESTINATION}/${basename}.opt.log")
         string(REPLACE ";" " " log_content "${command}")
         file(WRITE ${log_out_f} "${log_content}")
+        # Define custom command:
+        get_filename_component(basename ${in_f} NAME_WE)
+        set(asm_out_f "${arg_DESTINATION}/${basename}.s")
+        set(optreport_out_f "${arg_DESTINATION}/${basename}.opt")
+        get_optreport_outfile_flag(optreport_out_f_flag "${optreport_out_f}")
+        set(command
+            ${CMAKE_CXX_COMPILER} ${flags}
+            ${optreport_out_f_flag}
+            -o ${asm_out_f} -c ${in_f})
+        add_custom_command(
+            OUTPUT ${optreport_out_f} ${asm_out_f}
+            BYPRODUCTS ${log_out_f}
+            COMMAND ${command}
+            DEPENDS ${in_f}
+            WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+            COMMENT "Generating optimization report for ${basename}"
+            COMMAND_EXPAND_LISTS)
+        list(APPEND result ${optreport_out_f} ${asm_out_f} ${log_out_f})
     endforeach()
     # Add phony target to trigger all the generated
     # custom commands:
