3034573c14775c49af3142832717cedc4006a761
Federico Ficarelli <1379149+nazavode@users.noreply.github.com>
2025-04-19 12:10:49 +0200

    Fix lit suite

--- test/opt/Point4dSoaZipSum.cpp
+++ test/opt/Point4dSoaZipSum.cpp
@@ -1,3 +1,8 @@
+// RUN: mkdir -p %t
+// RUN: %cxx -march=skylake-avx512 -mtune=skylake-avx512 -O3 -DNDEBUG -std=c++17 -stdlib=libc++ \
+// RUN:      -fsave-optimization-record -foptimization-record-file=%t/remarks.yaml -o %t/out.o -c %s
+// RUN: %opt-report-summary < %t/remarks.yaml | FileCheck %s
+
 #include <vector>
 #include <zip.h>
 
@@ -8,7 +13,6 @@ struct Result {
     int w;
 };
 
-extern "C"
 Result Point4dSoaZipSum(const std::vector<int>& x, const std::vector<int>& y,
                         const std::vector<int>& z, const std::vector<int>& w) {
     int sum_x = 0;
@@ -22,6 +26,7 @@ Result Point4dSoaZipSum(const std::vector<int>& x, const std::vector<int>& y,
     const auto end =
         zip::make_iterator(zip::offset_iterator_tag{}, std::end(x), std::end(y), std::end(z), std::end(w));
 
+    // CHECK: PASSED(loop-vectorize) Point4dSoaZipSum.cpp:30 VectorizationFactor=8 InterleaveCount=2
     for(auto it = begin; it != end; ++it) {
         const auto [value_x, value_y, value_z, value_w] = *it;
         sum_x += value_x;
