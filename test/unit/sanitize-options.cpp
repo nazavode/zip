22239e095ba9c48ba9c59113572d64b56e11e2fa
Federico Ficarelli <1379149+nazavode@users.noreply.github.com>
2025-04-30 10:10:04 +0200

    ASAN angry with gtest on Apple arm64

--- test/unit/sanitize-options.cpp
+++ test/unit/sanitize-options.cpp
@@ -4,5 +4,8 @@
 extern "C" const char *__asan_default_options() {
     return "detect_stack_use_after_return=true:"
            "check_initialization_order=true:"
-           "detect_invalid_pointer_pairs=1";
+           "detect_invalid_pointer_pairs=1:"
+           // FIXME: gtest on macOS arm64 seems to be angry at
+           // REGISTER_TYPED_TEST_SUITE_P, needs to be investigated.
+           "detect_container_overflow=0";
 }
