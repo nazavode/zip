10a6765887c32d74374cbd9adac79c4533ddd28f
Federico Ficarelli <1379149+nazavode@users.noreply.github.com>
2025-04-30 09:47:57 +0200

    Remove unused cmake cache inits

--- cmake/cache/sanitize.cmake
+++
@@ -1,9 +0,0 @@
-# Enable Address SANitizer (ASAN) and Undefined Behaviour Sanitizer (UBSAN) for Debug build type.
-
-set(sanitize "-fsanitize=address,undefined -fno-omit-frame-pointer -fno-optimize-sibling-calls -O1 -g")
-
-# Debug
-set(CMAKE_C_FLAGS_DEBUG   "${sanitize}" CACHE STRING "")
-set(CMAKE_CXX_FLAGS_DEBUG "${sanitize}" CACHE STRING "")
-
-unset(sanitize)
